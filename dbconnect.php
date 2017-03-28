d58ddd5fdc65f1160f5f17f767c500a2f0034b64
quangtv92 <trinhvanquangf1@gmail.com>
2017-03-28 23:12:10 +0700

    thay doi file db connect

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,30 @@
 <!doctype html>
 <html>
 <head>
+<link type="text/css" rel="stylesheet" href="css/style.css">
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>index</title>
 </head>
-<?php
-$conn= mysql_connect("localhost","root","","test");
-mysql_query("SET NAMES 'utf8'");
-	
-	?>
+
 <body>
+	<?php
+		echo "Hello";
+		$conn = mysqli_connect("localhost","root","","learningsql");
+	if(!$conn)
+		{echo "Lỗi kết nối";}
+	else { echo "connected to db";}
+	
+		mysqli_set_charset($conn,"utf8");
+		$sql="select * from EMPLOYEE";
+		$resuilt = mysqli_query($conn,$sql);
+	if(mysqli_num_rows($resuilt)>0)
+		{
+			while($row=mysqli_fetch_array($resuilt))
+			{
+				echo $row[0]." ".$row[2]." {$row[3]}  <br>";
+			}
+		}
+	else echo("Bảng không có giá trị.");
+	?>
 </body>
 </html>
